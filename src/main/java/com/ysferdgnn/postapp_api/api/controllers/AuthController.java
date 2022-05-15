90e82b19eaf13d72cc7d68eee47b6bb31242b71b
ysferdgnn <ysferdgnn@gmail.com>
2022-05-15 19:32:47 +0300

    add refresh endpoint

--- src/main/java/com/ysferdgnn/postapp_api/api/controllers/AuthController.java
+++ src/main/java/com/ysferdgnn/postapp_api/api/controllers/AuthController.java
@@ -2,7 +2,11 @@ package com.ysferdgnn.postapp_api.api.controllers;
 
 
 import com.ysferdgnn.postapp_api.api.Requests.LoginRequest;
+import com.ysferdgnn.postapp_api.api.Requests.RefreshRequest;
+import com.ysferdgnn.postapp_api.api.Responses.LoginResponse;
+import com.ysferdgnn.postapp_api.api.database.models.RefreshToken;
 import com.ysferdgnn.postapp_api.api.database.models.Users;
+import com.ysferdgnn.postapp_api.api.database.services.RefreshTokenService;
 import com.ysferdgnn.postapp_api.api.database.services.UsersService;
 import com.ysferdgnn.postapp_api.api.security.tokenProviders.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -27,25 +34,32 @@ public class AuthController {
     private JwtTokenProvider jwtTokenProvider;
     private UsersService usersService;
     private PasswordEncoder passwordEncoder;
+    private RefreshTokenService refreshTokenService;
 
-    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider,UsersService usersService,PasswordEncoder passwordEncoder) {
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider,UsersService usersService,PasswordEncoder passwordEncoder,
+                          RefreshTokenService refreshTokenService) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
         this.usersService=usersService;
         this.passwordEncoder=passwordEncoder;
+        this.refreshTokenService=refreshTokenService;
     }
 
     @PostMapping("/login")
-    public String login (@RequestBody LoginRequest loginRequest){
+    public LoginResponse login (@RequestBody LoginRequest loginRequest){
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken=
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword());
 
         Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
 
+        Users users = usersService.findByUsername(loginRequest.getUsername());
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        String jwtToken = jwtTokenProvider.generateJwtToken(authentication);
-        return  "Bearer "+jwtToken;
+        String jwtToken = "Bearer "+jwtTokenProvider.generateJwtToken(authentication);
+        String refreshToken =jwtTokenProvider.generateJwtTokenByUserId(users.getId()).getToken();
+
+        return new LoginResponse(jwtToken,users.getId(),refreshToken);
+
     }
 
     @PostMapping("/register")
@@ -61,4 +75,30 @@ public class AuthController {
        usersService.saveOneUsers(usersToSave);
        return new ResponseEntity<String>("User successfully registered",HttpStatus.OK);
     }
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponse> refresh(@RequestBody RefreshRequest refreshRequest){
+
+        Optional<RefreshToken> refreshToken = refreshTokenService.findByUserId(refreshRequest.getUsersId());
+
+        if(refreshToken.isEmpty()){
+            return  new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        Users users = refreshToken.get().getUsers();
+        if (!refreshToken.get().getToken().equals(refreshRequest.getRefreshToken()) || refreshTokenService.isRefreshTokenExpired(refreshToken.get())) {
+            return  new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+
+
+        String jwtToken = "Bearer "+jwtTokenProvider.generateJwtTokenByUsersId(users.getId());
+        System.out.println("Token yenilendi!"+ new Date().toString());
+        return new ResponseEntity<LoginResponse>( new LoginResponse(jwtToken,users.getId(),refreshToken.get().getToken()),HttpStatus.OK);
+        }
+
+
+
+
+
+
 }
